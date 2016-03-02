19a584f2bec18c5e588e5ccbc88b5c1479c06898
sma69 <sma69@njit.edu>
2016-03-02 18:27:22 -0500

    added the hw via upload

---
+++ phptest1.php
@@ -0,0 +1,18 @@
+
+
+<?php
+
+function rainbowColors() {
+ 
+$rainbow = array("red","orange","yellow","green","blue","indigo", "violet"); 
+ $colors = 7;
+ 
+  for ( $i = 0; $i < $colors; $i++)
+   {
+    echo "$rainbow[i] + \n<br>";
+   }
+}
+rainbowColors();
+?>
+
+
