f3fa7fba53d77b0d665accb364b5aecf132ee51f
sma69 <sma69@njit.edu>
2016-03-30 23:59:19 -0400

    the game finally works

--- RockPaperScissor.php
+++ RockPaperScissor.php
@@ -1,79 +1,38 @@
-#!/usr/bin/php
-<?
+
+<?php
 require_once("clientDB.php.inc");
+require_once("RockPaperScissor.php.inc");
 
 
 
-   private $db = new ClientDB();
-   private$player1 = db->getClientID($name);
-   $answer2($rock,$paper,$scissor);
-   private $p1win =  $player."wins";
-   private $p2win = "player 2 wins";
-   $request = $_POST['request'];
-   $answer = '';
+   $db = new ClientDB('connect.ini');
+   $rps = new RockPaperScissor();
    $rock = "rock";
    $paper = "paper";
    $scissor = "scissor";
-   $quit = "quit";
+
+   $hands = array($rock,$paper,$scissor);
+   $ans2;
+
+   $request = $_POST['request'];
+   $ans1 = $_POST['hand'];
+
    
-   public beats($answer1, $answer2)
-   {
-   	switch($answer1 == $rock)
-	{
-	  case ($answer2 == $paper):
-	      $answer $p2win."<p>";
-	      break;
-	  case ($answer2 == $scissor):
-	      $answer = $p1win."<p>";
-	      break;
-	  case ($answer2 == $rock):
-	      $answer = "Tie!<p>";
-	      break;
-	}    
-	switch($answer1 == $paper)
-	{
-	  case ($answer2 == $paper):
-	      $answer = "Tie!<p>";
-	      break;
-	  case ($answer2 == $scissor):
-	      $answer = $p2win."<p>";
-	  case ($answer2 == $rock):
-	      $answer = $p1win."<p>";
-	}
-	switch($answer1 == $scissor)
-	{
-	  case ($answer2 == $paper):
-	      $answer = $p2win."<p>";
-	  case ($answer2 == $scissor):
-	      $answer = $p1win."<p>";
-	  case ($answer2 == $rock):
-	      $answer = "Tie!<p>";
-	      break;
-	}
-	echo $answer;
-    }
-   
+
 
 switch($request)
 {
       case 'move':
       {
-         echo array_rand($answer2);
-         beats($answer1,$answer2);
+         
+         $key = array_rand($hands);
+         $ans2 = $hands[$key];
+         $rps->beats($ans1,$ans2);
 
       }
       case 'challenge':
       {
       }
-   }	    
-   
+}	      
 
-      $answer = '';
-      $rock = "rock";
-      $paper = "paper";
-      $scissor = "scissor";
-      $quit = "quit";
-      
-
-
-
+?>
